a34b1574a711734347b8ecf1bc23e60188b99737
Yamashita, Shinichi <ichi0g0y@gmail.com>
2023-11-18 07:21:25 +0900

    :wrench: new eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,13 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  ignorePatterns: ['**/dist/', '**/node_modules/', '.eslintrc.cjs', 'vite.config.ts', 'jest.config.js'],
+  ignorePatterns: [
+    '**/dist/',
+    '**/node_modules/',
+    '.eslintrc.cjs',
+    'vite.config.ts',
+    'jest.config.js',
+  ],
   env: { node: true },
   extends: [
     'eslint:recommended',
